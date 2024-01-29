914d329d421ad3e0ba11f7979e72b2949164106a
Melih Karadas <melihkaradas@useinsider.com>
2024-01-29 13:29:40 +0300

    iOS Fixes

--- ios/InsiderNotificationContent/NotificationViewController.m
+++ ios/InsiderNotificationContent/NotificationViewController.m
@@ -1,29 +1,64 @@
-//
-//  NotificationViewController.m
-//  InsiderNotificationContent
-//
-//  Created by Admin on 28/11/23.
-//
-
 #import "NotificationViewController.h"
-#import <UserNotifications/UserNotifications.h>
 #import <UserNotificationsUI/UserNotificationsUI.h>
+#import <InsiderMobileAdvancedNotification/iCarousel.h>
+#import <InsiderMobileAdvancedNotification/InsiderPushNotification.h>
 
-@interface NotificationViewController () <UNNotificationContentExtension>
-
-@property IBOutlet UILabel *label;
-
+@interface NotificationViewController () <UNNotificationContentExtension, iCarouselDelegate, iCarouselDataSource>
+@property (nonatomic, weak) IBOutlet iCarousel *carousel;
 @end
+
+// FIXME: Please change with your app group.
+static NSString *APP_GROUP = @"group.com.company.app";
 
 @implementation NotificationViewController
 
-- (void)viewDidLoad {
+@synthesize carousel;
+
+-(void)viewDidLoad {
     [super viewDidLoad];
-    // Do any required interface initialization here.
 }
 
-- (void)didReceiveNotification:(UNNotification *)notification {
-    self.label.text = notification.request.content.body;
+-(void)viewWillDisappear:(BOOL)animated {
+    [InsiderPushNotification setTimeAttribution];
+}
+
+-(void)didReceiveNotification:(UNNotification *)notification {
+    [InsiderPushNotification interactivePushLoad:APP_GROUP superView:self.view notification:notification];
+
+    carousel.type = iCarouselTypeRotary;
+    [carousel reloadData];
+
+    [InsiderPushNotification interactivePushDidReceiveNotification];
+}
+
+-(NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel {
+    return [InsiderPushNotification getNumberOfSlide];
+}
+
+-(UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view {
+    return [InsiderPushNotification getSlide:index reusingView:view superView:self.view];
+}
+
+-(void)dealloc {
+    carousel.delegate = nil;
+    carousel.dataSource = nil;
+}
+
+-(CGFloat)carouselItemWidth:(iCarousel *)carousel {
+    return [InsiderPushNotification getItemWidth];
+}
+
+-(void)didReceiveNotificationResponse:(UNNotificationResponse *)response
+                     completionHandler:(void (^)(UNNotificationContentExtensionResponseOption option))completion {
+    if ([response.actionIdentifier isEqualToString:@"insider_int_push_next"]){
+        [carousel scrollToItemAtIndex:[InsiderPushNotification didReceiveNotificationResponse:[carousel currentItemIndex]] animated:true];
+
+        completion(UNNotificationContentExtensionResponseOptionDoNotDismiss);
+    } else {
+        [InsiderPushNotification logPlaceholderClick:response];
+
+        completion(UNNotificationContentExtensionResponseOptionDismissAndForwardAction);
+    }
 }
 
 @end
--- ios/InsiderNotificationService/NotificationService.h
+++ ios/InsiderNotificationService/NotificationService.h
@@ -1,44 +1,5 @@
-//
-//  NotificationService.h
-//  InsiderNotificationService
-//
-//  Created by Admin on 28/11/23.
-//
-
-#import <InsiderMobileAdvancedNotification/InsiderPushNotification.h>
+#import <UserNotifications/UserNotifications.h>
 
 @interface NotificationService : UNNotificationServiceExtension
-
-@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
-@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
-
-@end
-
-
-// FIXME: Please change with your app group.
-static NSString *APP_GROUP = @"group.com.KAI.KeretaApiiOS.KAI-Access";
-
-@implementation NotificationService
-
--(void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
-    self.contentHandler = contentHandler;
-    self.bestAttemptContent = [request.content mutableCopy];
-
-    // MARK: You can customize these.
-    NSString *nextButtonText = @">>";
-    NSString *goToAppText = @"Launch App";
-
-    [InsiderPushNotification showInsiderRichPush:request appGroup:APP_GROUP nextButtonText:nextButtonText goToAppText:goToAppText success:^(UNNotificationAttachment *attachment) {
-        if (attachment) {
-            self.bestAttemptContent.attachments = [NSArray arrayWithObject:attachment];
-        }
-
-        self.contentHandler(self.bestAttemptContent);
-    }];
-}
-
--(void)serviceExtensionTimeWillExpire {
-    self.contentHandler(self.bestAttemptContent);
-}
 
 @end
